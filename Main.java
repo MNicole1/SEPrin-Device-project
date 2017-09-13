ada7ceffaf2f271aa58ffcd5910e58092fd9f8c6
MNicole1 <mnicole1@msudenver.edu>
2017-09-12 18:05:38 -0600

    Run 10k trials in main.

--- Main.java
+++ Main.java
@@ -1,8 +1,20 @@
 public class Main {
 
     public static void main(String[] args) {
+        boolean allPass = true;
+
         System.out.println("Testing unlock");
-        Device dev = new Device(4, 2);
-        System.out.println(FourBitTwoDisclosureDeviceUnlocker.unlock(dev));
+        for (int i = 0; i < 10000; i++) {
+            Device dev = new Device(4, 2);
+            if (!FourBitTwoDisclosureDeviceUnlocker.unlock(dev)) {
+                System.out.println("Failure on try #"+ i);
+                allPass = false;
+            }
+            System.out.println(i);
+        }
+
+        if (allPass) {
+            System.out.println("All 10000 tests passed!");
+        }
     }
 }
