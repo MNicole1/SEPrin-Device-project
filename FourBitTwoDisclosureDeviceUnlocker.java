caf132b4d99ae8b52fd266c684438695ace43237
MNicole1 <mnicole1@msudenver.edu>
2017-09-16 13:09:20 -0600

    Some formatting changes.
    * Alphabetized names
    * Whitespacing
    * Expanding on Alex's comment for Unlock

--- FourBitTwoDisclosureDeviceUnlocker.java
+++ FourBitTwoDisclosureDeviceUnlocker.java
@@ -2,11 +2,11 @@
  * Solution development for 4-bit/2-disclosure device.
  *
  * @author Dr. Jody Paul - API
- * @author Marco Nicoletti
  * @author Anthony Ales
  * @author Michael Brown
  * @author Daniel Collier
  * @author Alex Jones
+ * @author Marco Nicoletti
  * @version 1.1.5
  * @see Device
  * @see <a href="../projectDescription.html">Project Description</a>
@@ -23,22 +23,29 @@ public class FourBitTwoDisclosureDeviceUnlocker extends DeviceUnlocker {
      */
     public static boolean unlock(final Device dev) {
         log(null); // Clear trace
-        boolean unlocked = false; // set to true if all bits are identical
+        boolean unlocked = false;
+
         for (int i = 0; i < 100; i++) {
-            log("Cycle #" + i);
+            log("Try #" + i);
             log("spin()");
+
+            // Set unlocked to true if all bits are identical
             unlocked = dev.spin();
             if (unlocked) {
                 log("Unlocked after " + i + " tries.");
                 break;
             }
+
             log("peek(\"??  \")");
             CharSequence peekResult = dev.peek("??  ");
             log("peek result: \"" + peekResult + "\"");
+
             log("poke(\"TT  \")");
             dev.poke("TT  ");
         }
 
+        // By now unlocked is true if we were successful in unlocking the device
+        // and false if we reached 100 tries without success.
         return unlocked;
     }
 
