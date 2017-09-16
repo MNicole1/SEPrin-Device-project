edb387001878f6099ae6d1b307a7cdcfb37c6152
MNicole1 <mnicole1@msudenver.edu>
2017-09-16 15:23:37 -0600

    Changes for stats collection

--- FourBitTwoDisclosureDeviceUnlocker.java
+++ FourBitTwoDisclosureDeviceUnlocker.java
@@ -16,6 +16,8 @@ import java.util.List;
  * @see <a href="../projectDescription.html">Project Description</a>
  */
 public class FourBitTwoDisclosureDeviceUnlocker extends DeviceUnlocker {
+    public static int[] spins = new int[100];
+
     /**
      * Unlocks a resource controlled by a 4-bit/2-disclosure device.
      * Behavior is unspecified if parameter is not a reference to a valid
@@ -26,27 +28,21 @@ public class FourBitTwoDisclosureDeviceUnlocker extends DeviceUnlocker {
      *         device are now identical); false otherwise
      */
     public static boolean unlock (final Device dev) {
-        log(null); // Clear trace
         boolean unlocked = false;
 
         for (int i = 0; i < 100; i++) {
-            log("Try #" + i);
-            log("spin()");
 
             // Set unlocked to true if all bits are identical
             unlocked = dev.spin();
             if (unlocked) {
-                log("Unlocked after " + i + " tries.");
+                spins[i]++;
                 break;
             }
 
             String peekPattern = shufflePattern('?', '?', '-', '-');
-            log("peek(\"" + peekPattern + "\")");
             CharSequence peekResult = dev.peek(peekPattern);
-            log("peek result: \"" + peekResult + "\"");
 
             String pokePattern = peekPattern.replace('?', Device.VALUE_TRUE);
-            log("poke(\"" + pokePattern + "\")");
             dev.poke(pokePattern);
         }
 
--- Main.java
+++ Main.java
@@ -4,19 +4,24 @@ public class Main {
         boolean allPass = true;
 
         System.out.println("Testing unlock");
-        int numTests = 100;
+        int numTests = 1000000000;
         for (int i = 0; i < numTests; i++) {
             Device dev = new Device(4, 2);
             if (!FourBitTwoDisclosureDeviceUnlocker.unlock(dev)) {
                 System.out.println("Failure on try #"+ i);
                 allPass = false;
             }
-            String trace = FourBitTwoDisclosureDeviceUnlocker.showTrace();
-            System.out.println(trace);
         }
 
         if (allPass) {
             System.out.println("All " + numTests + " tests passed!");
         }
+
+        int[] spins = FourBitTwoDisclosureDeviceUnlocker.spins;
+        System.out.print("frequencydata={{0," + spins[0] + "}");
+        for (int i = 1; i < spins.length; i++) {
+            System.out.printf(",{%d,%d}", i, spins[i]);
+        }
+        System.out.println("}");
     }
 }
